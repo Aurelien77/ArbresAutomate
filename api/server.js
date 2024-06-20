7e77adbaa0ba7b9edfc294923e0a9785ea7a3446
John Doe <johndoe@gmail.com>
2024-06-20 11:32:16 +0200

    test

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../Projet')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../Projet/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.get('/file-content', (req, res) => {
